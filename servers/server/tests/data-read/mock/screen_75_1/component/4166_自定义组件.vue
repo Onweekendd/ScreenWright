0f701dc004b86e39619fb0c5c664cba84cee3d52
onweekend <weijianwang74@gmail.com>
2026-09-11 02:56:15 -0700

    test(server): 将 data-read 的 mock 工作区作为普通文件入库
    该目录原带嵌套 .git，被主仓库记录为子模块指针而非文件内容，
    CI 全新 checkout 后为空目录，106 条用例因 info.json 缺失失败

---
+++ servers/server/tests/data-read/mock/screen_75_1/component/4166_自定义组件.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fun-card">{{ info.title }}</div>
+</template>
+
+<script>
+/**
+ * @param {import('@vue-part').VuePartInfo} info
+ * @returns {import('@vue-part').Vue2ComponentOptions}
+ * @__vp_types__
+ */
+function generate(info) {
+  return {
+    name: "fun-card",
+    data() {
+      return { title: info.title };
+    }
+  };
+}
+
+export default generate(/** @type {any} */ ({})); // @__vp_anchor__
+</script>
+
+<style>
+.fun-card {
+  color: #fff;
+}
+</style>
